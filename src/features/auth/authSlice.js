daafb07d5b776508b902cb2753ae915668c14c83
Bello Babakolo <fleetbeekay@gmail.com>
2021-04-12 06:24:38 +0100

    Initial setup

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,62 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { authAPI } from './counterAPI';
+
+const initialState = {
+  authToken: null,
+  loggedIn: false,
+  status: 'idle',
+};
+
+export const registerUserAsync = createAsyncThunk(
+  'auth/registerUser',
+  async () => {
+    const response = await authAPI('registration');
+    return response.data;
+  }
+);
+
+export const loginUserAsync = createAsyncThunk(
+    'auth/loginUser',
+    async () => {
+      const response = await authAPI('login');
+      return response.data;
+    }
+);
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.authToken = null;
+      state.loggedIn = false;
+      state.status = 'idle';
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUserAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(registerUserAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.authToken = action.payload;
+        state.loggedIn = true
+      })
+      .addCase(loginUserAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(loginUserAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.authToken = action.payload;
+        state.loggedIn = true
+      });
+  },
+});
+
+export const { logout } = counterSlice.actions;
+
+export const isUserLogged = (state) => state.auth.loggedIn;
+
+export default counterSlice.reducer;
