e4e8ed79537c52e57ba68c667ecf77e4b90d92ad
Bello Babakolo <fleetbeekay@gmail.com>
2021-04-19 00:12:10 +0100

    Add styles to the intro page!

--- src/features/measurements/postmeasurementwizard/Intro.js
+++ src/features/measurements/postmeasurementwizard/Intro.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Title from './Title';
+
+import style from './addMeasurement.module.css';
 
 const Intro = ({ handleNextForm }) => (
-  <div>
-    <p>Welcome back! Let's add your measurement for today.</p>
-    <button onClick={() => handleNextForm()}>Go</button>
+  <div className={style.intro}>
+    <Title />
+    <div className={style.introText}>
+      <p>
+        Welcome to the Task Tracker App! Let's add the number of hours expended 
+        on your pre-determined daily routines!
+      </p>
+    </div>
+    <button type="button" className={style.introBtn} onClick={() => handleNextForm()}>Go</button>
   </div>
 );
 
